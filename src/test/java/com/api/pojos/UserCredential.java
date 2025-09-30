3bacb698a865d1e5ae5250d21e7f64f7cd8ad6e8
RanjaniManuel <ranjanimanuel@gmail.com>
2025-09-30 14:39:49 -0400

    Updated pom.xml file with version 16
    Used Record feature of java and converted all the POJO classes into
    Records in the com.api.pojo

--- src/test/java/com/api/pojos/UserCredential.java
+++ src/test/java/com/api/pojos/UserCredential.java
@@ -1,29 +1,5 @@
 package com.api.pojos;
 
-public class UserCredential {
-    private String username;
-    private String password;
-	public UserCredential(String username, String password) {
-		super();
-		this.username = username;
-		this.password = password;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	@Override
-	public String toString() {
-		return "UserCredential [username=" + username + ", password=" + password + "]";
-	}
-	
-   
+public record UserCredential(String username, String password) {
+
 }
--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -1,5 +1,10 @@
 package com.api.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import com.api.constants.Role;
@@ -11,24 +16,37 @@ import com.api.pojos.Problem;
 import com.api.utils.SpecUtil;
 
 import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class CreateJobAPITest {
 	@Test
 	public void createJobApi() {
+		Random random = new Random();
+        
+        // Minimum 14-digit number
+        long min = 10000000000000L; 
+        long max = 99999999999999L;        
+        long number = min + (long)(random.nextDouble() * (max - min));
+        
 		Customer customer=new Customer("Ahi", "Rana", "9874563215", "8956231452", "ahi@gmail.com", "rana@gmail.com");
 		CustomerAddress customerAddress=new CustomerAddress("789", "Sai parivar", "M N Nmbiyar", "good Area", "Madurai", "987456", "India", "TamilNadu");
-		CustomerProduct customerProduct=new CustomerProduct("2025-04-08T04:00:00.000Z", "102499984280289", "102499984280280", "102499984280289", "2025-04-08T04:00:00.000Z", 3, 3);
+		CustomerProduct customerProduct=new CustomerProduct("2025-04-08T04:00:00.000Z", String.valueOf(number),  String.valueOf(number),  String.valueOf(number) , "2025-04-08T04:00:00.000Z", 3, 3);
 		
 		Problem problem=new Problem(1, "Slow");
-		Problem[] problemArray=new Problem[1];
-		problemArray[0]=problem;
+		List<Problem> problemArray=new ArrayList<Problem>();
+		problemArray.add(problem);
 		CreateJobPojo createJobPojo=new CreateJobPojo(0, 2, 1, 2, customer, customerAddress, customerProduct, problemArray);
 		 RestAssured.given()
 		 		.spec(SpecUtil.requestSpecWithAuth(Role.FD,createJobPojo))
 		 		.when()
 		 			.post("job/create")
-		 		.then()
-		 		.spec(SpecUtil.responseSpec_OK());
+		 		.then()		 		
+		 		.spec(SpecUtil.responseSpec_OK())
+		 		.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/createJobAPIResponseSchema.json"))
+		 		.body("message", Matchers.equalTo("Job created successfully. "))
+		 		.body("data.mst_service_location_id", Matchers.equalTo(1))
+		 		.body("data.job_number", Matchers.startsWith("JOB_"));
+		 	
 		 		
 	}
 
