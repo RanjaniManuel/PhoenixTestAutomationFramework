fc499dac73c1ba9f70d9e8d95f19addfff15b2a1
RanjaniManuel <ranjanimanuel@gmail.com>
2025-11-19 13:31:49 -0500

    Updated the DatabaseManager to use HikariCP library!!!

--- src/test/java/com/database/DatabaseManager.java
+++ src/test/java/com/database/DatabaseManager.java
@@ -1,35 +1,62 @@
 package com.database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.api.utils.ConfigManager;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DatabaseManager {
 
 	private static final String DB_URL = ConfigManager.getProperty("DB_URL");
 	private static final String DB_USER_NAME = ConfigManager.getProperty("DB_USER_NAME");
 	private static final String DB_PASSWORD = ConfigManager.getProperty("DB_PASSWORD");
-	private static volatile Connection connection; // Any update that happens to this variable
-	// All the thread will be aware of it
+	private static final int MAXIMUM_POOL_SIZE = Integer.parseInt(  ConfigManager.getProperty("MAXIMUM_POOL_SIZE"));
+	private static final int CONNECTION_TIMEOUT_IN_SECS = Integer.parseInt( ConfigManager.getProperty("CONNECTION_TIMEOUT_IN_SECS"));
+	private static final int MINIMUM_IDLE_COUNT =Integer.parseInt(  ConfigManager.getProperty("MINIMUM_IDLE_COUNT"));
+	private static final int MAX_LIFE_TIME_IN_MINS =Integer.parseInt(  ConfigManager.getProperty("MAX_LIFE_TIME_IN_MINS"));
+	private static final String HIKARI_CP_POOL_NAME = ConfigManager.getProperty("HIKARI_CP_POOL_NAME");
+	private static volatile Connection connection; 
+	private static HikariConfig hikariConfig;
+	private static volatile HikariDataSource hikariDataSource;
 
 	private DatabaseManager() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public  static void  createConnection() throws SQLException {
-		if (connection == null) {
+	private  static void  initializePool() {
+		if (hikariDataSource == null) {
 			synchronized (DatabaseManager.class) {
-				if (connection == null) {
-					connection = DriverManager.getConnection(DB_URL, DB_USER_NAME, DB_PASSWORD);
-
+				if (hikariDataSource == null) {
+					
+					hikariConfig=new HikariConfig();
+					hikariConfig.setJdbcUrl(DB_URL);
+					hikariConfig.setUsername(DB_USER_NAME);
+					hikariConfig.setPassword(DB_PASSWORD);
+					hikariConfig.setMaximumPoolSize(MAXIMUM_POOL_SIZE);
+					hikariConfig.setConnectionTimeout(CONNECTION_TIMEOUT_IN_SECS*1000);
+					hikariConfig.setMinimumIdle(MINIMUM_IDLE_COUNT);
+					hikariConfig.setMaxLifetime(MAX_LIFE_TIME_IN_MINS*60*1000);
+					hikariConfig.setPoolName(HIKARI_CP_POOL_NAME);
+					
+					hikariDataSource=new HikariDataSource(hikariConfig);
 				}
 			}
 			System.out.println(connection);
 			//return connection;
 
 		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		if(hikariDataSource==null)
+			initializePool();
+		else if(hikariDataSource.isClosed())
+			throw new SQLException("Hikari Data Source is Closed");
+			
+		connection=hikariDataSource.getConnection();
+		return connection;
 	}
 
 }
--- src/test/java/com/database/Demo.java
+++ src/test/java/com/database/Demo.java
@@ -1,17 +1,28 @@
 package com.database;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Demo {
 	
 	public static void main(String[] args) throws SQLException {
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
+		Connection connection = DatabaseManager.getConnection();
+		//System.out.println(connection);
+		
+	Statement statement = connection.createStatement();
+		
+		ResultSet restultSet = statement.executeQuery("select first_name, last_name, mobile_number  from tr_customer ;");
+	
+		while(restultSet.next()) {
+			System.out.println(restultSet.getString("first_name"));
+			System.out.println(restultSet.getString("last_name"));
+			System.out.println(restultSet.getString("mobile_number"));
+		}
+		
+		
+		
 	}
 
 }
